ade5227bc7f98ba81b5b199769d32af1d7bf8d08
carlos roberto roballo mello junior <www.juniorsolo@hotmail.com>
2021-06-21 19:27:28 -0300

    final!

--- src/main/App.java
+++ src/main/App.java
@@ -1,7 +1,6 @@
 package main;
 public class App {
     public static void main(String[] args) throws Exception {
-        new EscolheAdversario();
-         
+        new EscolheAdversario();       
     }
 }
